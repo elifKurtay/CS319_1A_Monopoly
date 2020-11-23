313c84de5ec5158bbae042cc2b4bdf9bd361d85b
Elif Kurtay <elif.kurtay00@gmail.com>
2020-11-23 21:10:24 +0300

    added lombok

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,33 +1,15 @@
 package entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
+@Getter
+@Setter
 public class Player {
     private ArrayList<Property> properties;
     private int money;
     private int getOutOfJailFreeCount;
 
-    public void setProperties(ArrayList<Property> properties) {
-        this.properties = properties;
-    }
-
-    public ArrayList<Property> getProperties() {
-        return properties;
-    }
-
-    public void setGetOutOfJailFreeCount(int getOutOfJailFreeCount) {
-        this.getOutOfJailFreeCount = getOutOfJailFreeCount;
-    }
-
-    public void setMoney(int money) {
-        this.money = money;
-    }
-
-    public int getGetOutOfJailFreeCount() {
-        return getOutOfJailFreeCount;
-    }
-
-    public int getMoney() {
-        return money;
-    }
 }
