31eb03f8318ae1f5273eb4a260641dc55ed64c75
Mustafa Goktan Gudukbay <goktan.gudukbay@ug.bilkent.edu.tr>
2020-11-25 23:18:35 +0300

    Change Main package name.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ui.MainMenuController;
+import frontend.MainMenuController;
 
 public class Main extends Application {
 
