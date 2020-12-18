f827bf41100d394e140160a0b4eea75d9cc42be4
AtakanDonmez <atk.donmez@gmail.com>
2020-12-18 15:27:06 +0300

    Auction changes

--- src/bank/Auction.java
+++ src/bank/Auction.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 public class Auction extends Observable{
     private final Property auctionedProperty;
 
+    private int[] bids;
     private int highestBid;
     private Player highestBidder;
 
@@ -20,12 +21,29 @@ public class Auction extends Observable{
         this.setState(1);
         highestBid = 0;
         highestBidder = null;
+        bids = new int[4];
+        for (int i = 0; i < 4; i++) {
+            bids[i] = 0;
+        }
     }
 
-    void bid(@NotNull Player bidder, int bid) {
+    void bid(@NotNull Player bidder, int bid, int playerNum) {
+        bids[playerNum] = bid;
         if(bid > highestBid) {
             this.highestBid = bid;
             this.highestBidder = bidder;
+        }
+    }
+
+    void fold(@NotNull Player bidder, int playerNum){
+        bids[playerNum] = -1;
+        int activeBidders = 0;
+        for (int i = 0; i < 4; i++) {
+            if (bids[i] > -1) activeBidders++;
+        }
+
+        if (activeBidders == 1){
+            closeAuction();
         }
     }
 
